3fd599d71f810de6062baeaea4e22f2b713ca05b
PA-Delaunay <40235634+PA-Delaunay@users.noreply.github.com>
2019-01-07 20:55:59 +0100

    Update inscription.php

--- inscription.php
+++ inscription.php
@@ -12,20 +12,19 @@
 		?>
 		<form method ="get" action ="retourInscriptionphp"> 
 			<p> 
+				Login : <input type = "text" name="login" /> </br>
+				Mot de passe : <input type = "text" name="password" /> </br> 
+				Nom :  <input type = "text" name="nom" /> </br>
+				Prenom : <input type = "text" name="prenom" /> </br>
+				Date de naissance :  <input type = "text" name="DateNaissance" /> </br>
+				Genre :  <input type = "text" name="genre" /> </br>
 				GroupeSanguin :  <input type = "text" name="groupeSanguin" /> </br>
 				Rhesus :  <input type = "text" name="rhesus" /> </br>
-				Genre :  <input type = "text" name="genre" /> </br>
-				Date de naissance :  <input type = "text" name="DateNaissance" /> </br>
-				nom :  <input type = "text" name="nom" /> </br>
-				prenom : <input type = "text" name="prenom" /> </br>
-				 <input type = "text" name="login" /> </br>
-				login : <input type = "text" name="login" /> </br>
-				mot de passe : <input type = "text" name="password" /> </br> 
 				<input type ="submit" value ="connexion" /> 
 			</p>
 		</form>
 
-		<?php echo "<a href ='Page1.php'> S'incrire </a>; 
+		<?php echo "<a href ='index.php'> S'incrire </a>; 
 
 
 		?> 
