3f3cde1bceac5dfb6767fb212c1eec2cbb2e10a3
PA-Delaunay <40235634+PA-Delaunay@users.noreply.github.com>
2019-01-07 22:16:40 +0100

    Update retourInscription.php

--- retourInscription.php
+++ retourInscription.php
@@ -7,34 +7,50 @@
 	</head> 
 	<body> 
 		<?php echo "Connexion";
-		// start a session 
-		session_start();
-
-		$nom = $_GET['nom']; 
-		$prenom = $_GET['prenom']; 
-		$login = $_GET['login']; 
-		$password=$_GET['password']; 
-		$rhesus=$_GET['rhesus']; 
-		$genre=$_GET['genre']; 
-
-
-		$_SESSION['nom']=$nom; 
-		$_SESSION['prenom']=$prenom; 
-		$_SESSION['login']=$login; 
-		$_SESSION['password']=$password; 
-		$_SESSION['rhesus']=$rhesus; 
-		$_SESSION['genre']=$genre; 
-
-		$nom = $_SESSION['nom']; 
-		$prenom = $_SESSION['prenom']; 
-		$login = $_SESSION['login']; 
-		$password = $_SESSION['password']; 
-		$rhesus = $_SESSION['rhesus']; 
-		$genre = $_SESSION['genre']; 
-
-
-		//Connexion à la base :) 
-
+			// start a session 
+			session_start();
+			if(isset($_GET["login"])&&
+			   isset($_GET["mdp"])&&
+			   isset($_GET["validate"])&&
+			   isset($_GET["nom"])&&
+			   isset($_GET["prenom"])&&
+			   isset($_GET["dateNaissance"])&&
+			   isset($_GET["genre"])&&
+			   isset($_GET["groupeSanguin"])&&
+			   isset($_GET["rhesus"])){
+				$login = $_GET["login"];
+				$mdp = $_GET["mdp"];
+				$validate = $_GET["validate"];
+				$nom = $_GET["nom"];
+				$prenom = $_GET["prenom"];
+				$dateNaissance = $_GET["dateNaissance"];
+				$genre = $_GET["genre"];
+				$groupeSanguin = $_GET["groupeSanguin"];
+				$rhesus = $_GET["rhesus"];
+				if($mdp==$validate){
+					$connection = pg_connect("host=localhost dbname=dondusang user=root password = network")
+					or die ('Connexion impossible : '.pg_last_error());
+					$connection->set_charset("utf8");
+					$sqlQuery = "select login from Donneur";
+					$sqlLogin = pg_query($sqlQuery) or die ('Echex de la requête : ". pg_last_error());
+					if($login != $sqlLogin){
+						$sqlQuery = "insert into Donneur (login ,mdp, nom, prenom, genre, dateN, groupeS, rhesus)
+						values(".$login.",".$mdp.",".$nom.",".$prenom.",".$genre.",".$dateNaissance.",".$groupeSanguin.",".$rhesus.");";
+						$result = pg_query($sqlQuery)
+						or die ('Echec de la requete : '. pg_last_error());
+						echo ("<a href=\"connexion.php\"> Compte créé</a>");
+						exit();
+					} else {
+						echo ("<a href=\"inscription.php\"> Login déjà utilisé</a>");
+						exit();
+				} else {
+					echo ("<a href=\"inscription.php\"> Saisie incorrecte</a>");
+					exit();
+				}
+			} else {
+				echo ("<a href=\"inscription.php\"> Saisie incorrecte</a>");
+				exit();
+			}
 		?> 
 	</body>
 </html>
