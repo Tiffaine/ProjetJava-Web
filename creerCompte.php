6ca0f6f2afc47cf159e0df62e50ddf24bb52d6ae
PA-Delaunay <40235634+PA-Delaunay@users.noreply.github.com>
2019-01-07 20:50:59 +0100

    Update creerCompte.php

--- creerCompte.php
+++ creerCompte.php
@@ -1,3 +1,20 @@
-<php?
+<html>
+	<head>
+		<meta charset="utf-8"/>
+		<link rel="stylesheet" href="style1.css"/>
+		<title> Connexion </title>
+	</head>
+	<body>
+		<form method ="get" action ="retourConnexion.php"> 
+			<p> 
+				login : <input type = "text" name="login" /> </br>
+				mot de passe : <input type = "password" name="password" /> </br>
+				validate : <input type = "password" name="validate" /> </br>
+				<input type ="submit" value ="créer un compte" /> 
+			</p>
+		</form>
+		<?php echo "<a href='index.php'> Retour </a>";
+		?>
+	</body>
+</html>
 
-?>
