cedd73fcbf2fd4e09f07afd57651d5cbc5792ca7
PA-Delaunay <40235634+PA-Delaunay@users.noreply.github.com>
2019-01-09 15:00:38 +0100

    Update retourConnexion.php

--- retourConnexion.php
+++ retourConnexion.php
@@ -16,55 +16,55 @@
 				$connection = new pg_connect(host=localhost dbname=dondusang user=root password=network)
 				or die('Connexion impossible : '. pg_last_error());
 				//$connection->set_charset("utf8");
-				$sqlQuery = "select login, motDePasse from Donneur d where d.login like '" . $login."' and d.motDePasse like '" . $mdp."'";
+				$sqlQuery = "select idUtilisateur, type from Utilisateur u where u.login like '" . $login."' and u.mdp like '" . $mdp."'";
 				$result = pg_query($sqlQuery)
 				or die('Echec de la requête : '.pg_last_error());
 				if(!$result){
-					echo ("<br> la requête ne s'est pas exécutée </br>");
-					echo ("Veuillez remplir tous les champs ! <br />");
-					echo ("<a href=\"index.php\"> Retour à l'index</a>");
+					echo ("Login ou Password invalide !</br>");
+					echo ("<a href=\"connexion.php\"> Retour à la connexion </a>");
 					exit();
 				} else {
-					//echo ("<br> La requête a aboutie </br>");
 					$resultat = $result->fetch_assoc();
-					//echo ("<br> " . $resultat . "</br>");
-					if($resultat === NULL) {
-						echo ("Login ou mot de passe invalide ! <br />");
-						echo ("<a href=\"index.php\"> Retour à l'index</a>");
-						exit();
-					} else {
-						if ($resultat["numUtilisateur"] != NULL) {
+					if($resultat != NULL){
+						if($resultat["idUtilisateur"] != NULL){
 							session_start();
-							$_SESSION["numUtilisateur"] = $resultat["numUtilisateur"];
-							if ($resultat["numUtilisateur"] == '1') {
-								echo ("Vous êtes connecté en administrateur <br />");
-								echo ("<a href=\"menuAdministrateur.php\"> Aller au menu admin</a>");
+							$SESSION["idUtilisateur"] = $resultat["idUtilisateur"];
+							if($resultat["idUtilisateur"] == '1'){
+								echo ("Vous êtes connecté en tant qu'administrateur </br>");
+								echo ("<a href=\"menuAdministrateur.php\"> Menu administrateur </a>");
 								exit();
 							} else {
-								echo ("Vous êtes connecté <br />");
-								echo ("<a href=\"listeConcours.php\"> Aller au menu utilisateur</a>");
-								exit();
+								if($resultat["type"] == 'donneur'){
+									echo("Vous êtes connecté </br>");
+									echo ("<a href=\"menuDonneur.php\"> Menu </a>");
+									exit();
+								} else {
+									echo ("Login ou Password invalide !</br>");
+									echo ("<a href=\"connexion.php\"> Retour à la connexion </a>");
+									exit();
+								}
 							}
+						} else {
+							echo ("Login ou Password invalide !</br>");
+							echo ("<a href=\"connexion.php\"> Retour à la connexion </a>");
+							exit();
 						}
+					} else {
+						echo ("Login ou Password invalide !</br>");
+						echo ("<a href=\"connexion.php\"> Retour à la connexion </a>");
+						exit();
 					}
-					pg_free_result($result);
 				}
-				pg_close($dondusang);
 			} else {
-				echo ("<br> Veuillez remplir tous les champs ! </br>");
-				echo ("<a href=\"index.php\"> Retour à l'index</a>");
+				echo ("Login ou Password invalide !</br>");
+				echo ("<a href=\"connexion.php\"> Retour à la connexion </a>");
 				exit();
 			}
 		} else {
-			echo ("Veuillez remplir tous les champs ! <br />");
-			echo ("<a href=\"index.php\"> Retour à l'index</a>");
+			echo ("Login ou Password invalide !</br>");
+			echo ("<a href=\"connexion.php\"> Retour à la connexion </a>");
 			exit();
 		}
-		?>
-		<?php echo "<a href ='index.php'> S'inscrire </a>"; 
-
-
-		?> 
 	</body>
 </html>
 
