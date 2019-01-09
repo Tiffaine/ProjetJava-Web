dfa0b4216562432248ddd5fbe238ffb2dda682e3
PA-Delaunay <40235634+PA-Delaunay@users.noreply.github.com>
2019-01-09 15:17:13 +0100

    Create menuAdministrateur.php

---
+++ menuAdministrateur.php
@@ -0,0 +1,21 @@
+
+<html>
+	<head> 
+		<meta charset="utf-8"/> 
+		<link rel="stylesheet" href="style1.css"/>
+		
+		<title> Menu Administrateur </title> 
+	</head> 
+	<body> 
+		<?php
+			session_start();
+			if($SESSION["idUtilisateur"] == '1'){
+				
+			} else {
+				echo ("Vous n'êtes pas autorisé à acceder à ce menu </br>");
+				echo ("<a href=\"index.php\"> Retour à l'accueil </a>");
+				exit();
+			}
+		?>
+	</body>
+</html>
