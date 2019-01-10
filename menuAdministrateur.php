b2669d005ef786d903a840ac16dbcb58558e8c62
PA-Delaunay <40235634+PA-Delaunay@users.noreply.github.com>
2019-01-10 16:56:17 +0100

    Update menuAdministrateur.php

--- menuAdministrateur.php
+++ menuAdministrateur.php
@@ -9,11 +9,32 @@
 	<body> 
 		<?php
 			session_start();
-			if($SESSION["idUtilisateur"] == '1'){
-				
+			$id = $SESSION["idUtilisateur"];
+			if(isset($id)){
+				if($id == '1'){
+					$connection = new pg_connect(host=localhost dbname=dondusang user=root password=network)
+					or die('Connexion impossible : '. pg_last_error());
+					$sqlQuery = "select idUtilisateur, nom, prenom from Utilisateur u when u.type like 'medecin'";
+					$result = pg_query($sqlQuery) or die ('Echec de la requête : '.pg_last_error());
+					if(!$result){
+						echo("Echec de la requête </br>");
+						echo("<a href=\"index.php\"> Retour </a>");
+						exit();
+					} else {
+						$listeMedecin = $result -> fetch_assoc();
+						do {
+							echo("<br> Dr ".$listeMedecin['nom']." ".$listeMedecin['prenom']." 
+							<a href=\"supprimerMedecin.php?idMedecin=".$listeMedecin['idUtilisateur']."\">Supprimer</a> </br>");
+						}while($listeMedecin);
+					}
+				} else {
+					echo ("Vous n'êtes pas autorisé à acceder à ce menu </br>");
+					echo ("<a href=\"index.php\"> Retour</a>");
+					exit();
+				}
 			} else {
 				echo ("Vous n'êtes pas autorisé à acceder à ce menu </br>");
-				echo ("<a href=\"index.php\"> Retour à l'accueil </a>");
+				echo ("<a href=\"index.php\"> Retour </a>");
 				exit();
 			}
 		?>
