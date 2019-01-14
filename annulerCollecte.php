04a24ca418997a3152053d7ff55bbc267ca535f7
Tiffaine <40236031+Tiffaine@users.noreply.github.com>
2019-01-14 14:44:27 +0100

    Update annulerCollecte.php

--- annulerCollecte.php
+++ annulerCollecte.php
@@ -10,38 +10,44 @@
 		<?php 
 			session_start(); 
 			//Connexion à la base de données 
-
+			session_start();
+			$id = $SESSION["idUtilisateur"];
+			if(isset($id)){
+				if($id != '1'){
 			
+					//Prendre la liste des collectes où l'utilisateur est donneur 
+					$maRequete="SELECT refCollecte FROM Donneur WHERE $SESSION["idUtilisateur"]=refUtilisateur";
+					$result=$connexion->query($maRequete); 
+					if(!$result){
+						echo("La requête ne s'est pas faite"); 
+						exit(); 
+					}
 
 
+					//On affiche un par un toutes les collectes où l'utilisateur est donneur
+					$listeCollecte=$result->fetch_assoc();
+					while($listeCollecte){
+						$idCollecte=$idCollecte["idCollecte"];
+						$date=$date["date"];
+						$heure=$heure["heure"];
+						$lieu=$lieu["lieu"];
+						echo "Collecte n° $idCollecte </br> Date $date </br> Heure $heure </br> Lieu $lieu </br> <a href='desinscriptionCollecte.php?idCollect=$idCollecte'> S incrire au concours </a> "; 
+						$listeCollecte=$result->fetch_assoc(); 
+					}
 
-			//Prendre la liste des collectes où l'utilisateur est donneur 
-			$maRequete="SELECT refCollecte FROM Donneur WHERE $SESSION["idUtilisateur"]=refUtilisateur";
-			$result=$connexion->query($maRequete); 
-			if(!$result){
-			echo("La requête ne s'est pas faite"); 
-			exit(); 
-		}
+					$listeCollecte=$result->fetch_assoc(); 
+					if($listeCollecte==0){ 
+						echo("Aucune collecte n'a été enregistrée"); 
+						echo("<a href=menuDonneur.php> Retour</a>"); 
+						exit(); 
 
-
-			//On affiche un par un toutes les collectes où l'utilisateur est donneur
-			$listeCollecte=$result->fetch_assoc();
-			while($listeCollecte){
-			$idCollecte=$idCollecte["idCollecte"];
-			$date=$date["date"];
-			$heure=$heure["heure"];
-			$lieu=$lieu["lieu"];
-		echo "Collecte n° $idCollecte </br> Date $date </br> Heure $heure </br> Lieu $lieu </br> <a href='desinscriptionCollecte.php?idCollect=$idCollecte'> S incrire au concours </a> "; 
-		$listeCollecte=$result->fetch_assoc(); 
-		}
-
-		$listeCollecte=$result->fetch_assoc(); 
-		if($listeCollecte==0){ 
-		echo("Aucune collecte n'a été enregistrée"); 
-		echo("<a href=menuDonneur.php> Retour</a>"); 
-		exit(); 
-
-	}
+					}
+				}
+				else {
+					echo ("<a href=\"index.php\"> Retour </a>");
+					exit();
+				}
+			}
 		?> 
 	</body>
 </html>
