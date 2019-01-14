3fbed79aeb02ce3db6c3510a4246fc80d9bc94e6
Tiffaine <40236031+Tiffaine@users.noreply.github.com>
2019-01-14 16:24:12 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,12 +5,12 @@
 		<title> Gestionnaire de Don du Sang - Welcome Page </title> 
 	</head> 
 	<body> 
-		<?php echo "Bienvenue sur le Gestionnaire de Don du Sang";
+		echo "Bienvenue sur le Gestionnaire de Don du Sang";
 		echo "<a href='connexion.php'> Connexion </a>"; 
 		echo "Pas encore de compte ? Créez-en un!";
 		echo "<a href='creerCompte.php'> Créer un compte </a>"; 
 
-		?> 
+		
 	</body>
 </html>
 
