215daf1b10903bf0d3d5b557671040d2728c9d15
Tiffaine <40236031+Tiffaine@users.noreply.github.com>
2019-01-14 14:53:44 +0100

    Update participerCollecte.php

--- participerCollecte.php
+++ participerCollecte.php
@@ -7,41 +7,38 @@
 	</head>
 
 	<body>
+		echo ("<a href=\"index.php\"> Retour </a>");
 		<?php 
 			session_start(); 
 			//Connexion à la base de données 
 
 
 
-
-
-
 			$maRequete="SELECT idCollecte,date,heure,lieu FROM Collecte";
 			$result=$connexion->query($maRequete); 
 			if(!$result){
-			echo("La requête ne s'est pas faite"); 
-			exit(); 
-		}
+				echo("La requête ne s'est pas faite"); 
+				exit(); 
+			}
 
 
 			//On affiche un par un toutes les collectes 
 			$listeCollecte=$result->fetch_assoc();
 			while($listeCollecte){
-			$idCollecte=$idCollecte["idCollecte"];
-			$date=$date["date"];
-			$heure=$heure["heure"];
-			$lieu=$lieu["lieu"];
-		echo "Collecte n° $idCollecte </br> Date $date </br> Heure $heure </br> Lieu $lieu </br> <a href='inscriptionCollecte.php?idCollect=$idCollecte'> S incrire au concours </a> "; 
-		$listeCollecte=$result->fetch_assoc(); 
-		}
+				$idCollecte=$idCollecte["idCollecte"];
+				$date=$date["date"];
+				$heure=$heure["heure"];
+				$lieu=$lieu["lieu"];
+				echo "Collecte n° $idCollecte </br> Date $date </br> Heure $heure </br> Lieu $lieu </br> <a href='inscriptionCollecte.php?idCollect=$idCollecte'> S incrire au concours </a> "; 
+				$listeCollecte=$result->fetch_assoc(); 
+			}
 
-		$listeCollecte=$result->fetch_assoc(); 
-		if($listeCollecte==0){ 
-		echo("Aucune collecte n'a été enregistrée"); 
-		echo("<a href=menuDonneur.php> Retour</a>"); 
-		exit(); 
-
-	}
+			$listeCollecte=$result->fetch_assoc(); 
+			if($listeCollecte==0){ 
+				echo("Aucune collecte n'a été enregistrée"); 
+				echo("<a href=menuDonneur.php> Retour</a>"); 
+				exit(); 
+			}
 		?> 
 	</body>
 </html>
