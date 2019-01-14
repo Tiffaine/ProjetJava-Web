85705de5c605f2a6a8877339accc8e72d346e008
Tiffaine <40236031+Tiffaine@users.noreply.github.com>
2019-01-14 15:59:00 +0100

    Update retourAnnulation.php

--- retourAnnulation.php
+++ retourAnnulation.php
@@ -12,15 +12,20 @@
 			//Connexion à la base de données 
 
 			
-
-
-
-			$maRequete="DELETE FROM Donneur WHERE refCollecte=$idCollecte and refUtilisateur=$SESSION["idUtilisateur"]";
-			$result=$connexion->query($maRequete); 
-			if(!$result){
-			echo("La requête ne s'est pas faite"); 
-			exit(); 
-			}
+			$connection = pg_connect("host=localhost dbname=dondusang user=root password = network")
+						or die ('Connexion impossible : '.pg_last_error());
+						$connection->set_charset("utf8");
+						$sqlQuery = "select login from Utilisateur";
+						$sqlLogin = pg_query($sqlQuery) 
+						or die ('Echec de la requête : '. pg_last_error());
+						if($login != $sqlLogin){
+							$sqlQuery = "DELETE FROM Donneur WHERE refCollecte=$idCollecte and refUtilisateur=$SESSION["idUtilisateur"]";
+							
+							$result = pg_query($sqlQuery);
+							or die ('Echec de la requete : '. pg_last_error());
+							echo ("<a href=\"menuAdministrateur.php\">Compte créé</a>");
+							exit();
+						}
 
 			echo("<a href=\"index.php\"> Retour à l'accueil </a>");
 			
