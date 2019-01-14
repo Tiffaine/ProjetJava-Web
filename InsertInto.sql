527d13211e42e71c07e2561ca48d7ce428f7c5c1
Tiffaine <40236031+Tiffaine@users.noreply.github.com>
2019-01-14 14:35:56 +0100

    Add files via upload

---
+++ InsertInto.sql
@@ -0,0 +1,9 @@
+INSERT INTO 'Utilisateur' VALUES ("donneur","dodo","lesAntilles<3","Do","Pierre",1995-12-31); 
+
+INSERT INTO 'Utilisateur' VALUES ("donneur","PA49","HdgOsh67","Delaunay","Paul-Adrien",1997-12-10); 
+
+
+INSERT INTO 'Utilisateur' VALUES ("medecin","ElDoctor","yoazdbGus87","Escobar","Pablo",1949-12-1); 
+
+INSERT INTO 'Utilisateur' VALUES ("medecin","Dr.Dupont","USèBS-34","Dupont","Jean-Claude",1973-09-24); 
+
