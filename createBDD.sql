7f5a7e42c07a3198955696237c290e5dc729d8c2
Tiffaine <40236031+Tiffaine@users.noreply.github.com>
2019-01-21 10:30:29 +0100

    Update createBDD.sql

--- createBDD.sql
+++ createBDD.sql
@@ -1,30 +1,32 @@
-
-
-DROP TABLE if exists Medecin
-DROP TABLE if exists Donneur
-DROP TABLE if exists Collecte
-DROP TABLE if exists Utilisateur
+DROP TABLE if exists Medecin;
+DROP TABLE if exists Donneur;
+DROP TABLE if exists Collecte;
+DROP TABLE if exists Utilisateur;
 
 CREATE TABLE Utilisateur(
-	idUtilisateur integer NOT NULL SERIAL PRIMARY KEY,
+	idUtilisateur SERIAL,
 	type varchar(20) NOT NULL,
 	login varchar(20) NOT NULL,
 	mdp varchar(20) NOT NULL,
 	nom varchar(30) NOT NULL,
 	prenom varchar(30) NOT NULL,
-	dateN DATE NOT NULL
-	)
+	dateN DATE NOT NULL,
+
+	PRIMARY KEY (idUtilisateur)
+	);
 
 CREATE TABLE Collecte(
-	idCollecte integer NOT NULL SERIAL PRIMARY KEY,
+	idCollecte SERIAL,
 	nbP integer,
 	nbMaxP integer NOT NULL,
 	nbL integer NOT NULL,
 	etat varchar(20) NOT NULL,
 	date DATE NOT NULL,
-	lieu varchar() NOT NULL,
-	description varchar()
-	)
+	lieu varchar(50) NOT NULL,
+	description varchar(50),
+
+	PRIMARY KEY (idCollecte)
+	);
 	
 CREATE TABLE Donneur(
 	refDonneur integer NOT NULL,
@@ -33,15 +35,17 @@ CREATE TABLE Donneur(
 	rhesus varchar(10) NOT NULL,
 	
 	FOREIGN KEY (refDonneur) REFERENCES Utilisateur(idUtilisateur),
-	FOREIGN KEY (refCollecte) REFERENCES Collecte(idCollecte)
-	)
+	FOREIGN KEY (refCollecte) REFERENCES Collecte(idCollecte),
+	PRIMARY KEY (refDonneur)
+	);
 
 CREATE TABLE Medecin(
 	refMedecin integer NOT NULL,
 	refCollecte integer NOT NULL,
 	
-	FOREIGN KEY(refDonneur) REFERENCES Utilisateur(idUtilisateur),
-	FOREIGN KEY(refCollecte) REFERENCES Collecte(idCollecte)
-	)	
+	FOREIGN KEY(refMedecin) REFERENCES Utilisateur(idUtilisateur),
+	FOREIGN KEY(refCollecte) REFERENCES Collecte(idCollecte),
+	PRIMARY KEY (refMedecin)
+	);
 
 
