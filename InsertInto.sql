a30445a9b9030964a4485fe6d80941a6a83c7016
Tiffaine <40236031+Tiffaine@users.noreply.github.com>
2019-01-21 10:42:01 +0100

    Update InsertInto.sql

--- InsertInto.sql
+++ InsertInto.sql
@@ -1,9 +1,9 @@
-INSERT INTO 'Utilisateur' VALUES ("donneur","dodo","lesAntilles<3","Do","Pierre",1995-12-31); 
+INSERT INTO Utilisateur (type,login,mdp,nom,prenom,dateN) VALUES ('donneur','dodo','lesAntilles<3','Do','Pierre','1995-12-31'); 
 
-INSERT INTO 'Utilisateur' VALUES ("donneur","PA49","HdgOsh67","Delaunay","Paul-Adrien",1997-12-10); 
+INSERT INTO Utilisateur (type,login,mdp,nom,prenom,dateN) VALUES ('donneur','PA49','HdgOsh67','Delaunay','Paul-Adrien','1997-12-10'); 
 
 
-INSERT INTO 'Utilisateur' VALUES ("medecin","ElDoctor","yoazdbGus87","Escobar","Pablo",1949-12-1); 
+INSERT INTO Utilisateur (type,login,mdp,nom,prenom,dateN) VALUES ('medecin','ElDoctor','yoazdbGus87','Escobar','Pablo','1949-12-1'); 
 
-INSERT INTO 'Utilisateur' VALUES ("medecin","Dr.Dupont","USèBS-34","Dupont","Jean-Claude",1973-09-24); 
+INSERT INTO Utilisateur (type,login,mdp,nom,prenom,dateN) VALUES ('medecin','Dr.Dupont','USèBS-34','Dupont','Jean-Claude','1973-09-24'); 
 
