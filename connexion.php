6a673a3cef8c10c167260e463f99c25ca4cd1db5
PA-Delaunay <40235634+PA-Delaunay@users.noreply.github.com>
2019-01-23 10:34:59 +0100

    Update connexion.php

--- connexion.php
+++ connexion.php
@@ -7,20 +7,22 @@
 	</head> 
 	<body> 
 		<br> Connexion </br>
-		<form method ="post" action ="retourConnexion.php"> 
-			<fieldset>
-            	<legend>login : </legend>
-                <input type = "text" name="login" />
-            </fieldset>
-            <fieldset>
-            	<legend>mot de passe : </legend>
-                <input type = "password" name="mdp" />
-            </fieldset>
-				<input type ="submit" value ="connexion" /> 
-		</form>
-
 		<a href ='index.php'> Retour </a>
-
+	
 	</body>
+	<form method ="post" action ="retourConnexion.php"> 
+		<fieldset>
+			<legend>login : </legend>
+			<input type = "text" name="login" />
+    		</fieldset>
+   		<fieldset>
+			<legend>mot de passe : </legend>
+			<input type = "password" name="mdp" />
+    		</fieldset>
+			<input type ="submit" name="submit" value ="se connecter" /> 
+	</form>
+
+		
+
 </html>
 
