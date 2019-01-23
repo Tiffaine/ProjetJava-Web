f160a877ecfaa9973046e09ef004eb6b5ac4caef
PA-Delaunay <40235634+PA-Delaunay@users.noreply.github.com>
2019-01-23 10:16:29 +0100

    Update retourConnexion.php

--- retourConnexion.php
+++ retourConnexion.php
@@ -1,26 +1,15 @@
-<html> 
-	<head> 
-		<meta charset="utf-8"/> 
-		<link rel="stylesheet" href="style1.css"/>
-		
-		<title> Page de connexion </title> 
-	</head> 
-	<body> 
-		<br> Connexion </br>
-		<?php
-		// start a session 
-		//session_start();
-		if(isset($_POST['login'] and $_POST['mdp']){
-			$login = $_GET['login'];
-			var_dump($login);
-			$mdp = $_GET['mdp'];
-			var_dump($mdp);
-		} else {
-			echo ("Login ou Password invalide !</br>");
-			echo ("<a href=\"connexion.php\"> Retour à la connexion </a>");
-			exit();
-		}
-		?>
-	</body>
-</html>
+<?php
+// start a session 
+//session_start();
+if(isset($_POST['login'] and $_POST['mdp']){
+	$login = $_GET['login'];
+	var_dump($login);
+	$mdp = $_GET['mdp'];
+	var_dump($mdp);
+} else {
+	echo ("Login ou Password invalide !</br>");
+	echo ("<a href=\"connexion.php\"> Retour à la connexion </a>");
+}
+?>
+
 
