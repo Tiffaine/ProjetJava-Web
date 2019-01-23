620de61d9ffd6c6cdb0fc72b772bb101c1b9038a
PA-Delaunay <40235634+PA-Delaunay@users.noreply.github.com>
2019-01-23 11:32:27 +0100

    Update retourConnexion.php

--- retourConnexion.php
+++ retourConnexion.php
@@ -1,34 +1,45 @@
 <?php
 // start a session 
 //session_start();
-if(isset($_POST['login'] and $_POST['mdp']){
-	$login = $_GET['login'];
-	var_dump($login);
-	$mdp = $_GET['mdp'];
-	var_dump($mdp);
+if(isset($_POST['login']) and isset($_POST['mdp'])){
+	$login = $_POST['login'];
+	//var_dump($login);
+	$mdp = $_POST['mdp'];
+	//var_dump($mdp);
 	if(($login != "") and ($mdp != "")){
+		//echo("Coucou\n");
 		$pgsql_conn = pg_connect("dbname=dondusang host=localhost user=admin password=projetgroupe4");
 		if(!$pgsql_conn){
 			echo(pg_last_error($pgsql_conn));
 			exit();
-		} else {
-			echo("Connexion etablie\n");
+		//} else {
+			//echo("Connexion etablie\n");
 		}
-		$sql_query = "select idUtilisateur, type from Utilisateur u where u.login like '" . $login."' and u.mdp like '" . $mdp."'";
+		$sql_query = "select idUtilisateur, type from Utilisateur u where u.login like '" . $login."' and u.mdp like '" . $mdp."';";
 		echo("requete id type : ".$sql_query);
 		$result = pg_query($pgsql_conn, $sql_query);
 		if(!$result){
 			echo("Login ou password invalide !\n");
+			echo ("<a href=\"connexion.php\"> Retour à la connexion </a>");
 			exit();
 		} else {
-			echo("resultat disponible\n");
+			//echo("resultat disponible\n");
+			$resultat = pg_fetch_assoc($result);
+			if($resultat != NULL){
+				echo("resultat exploitable\n");
+			} else {
+				echo("resultat nul\n");
+			}
 		}
 	} else {
 		echo ("Login ou Password invalide !</br>");
 		echo ("<a href=\"connexion.php\"> Retour à la connexion </a>");
+		exit();
+	}
 } else {
 	echo ("Login ou Password invalide !</br>");
 	echo ("<a href=\"connexion.php\"> Retour à la connexion </a>");
+	exit();
 }
 ?>
 
