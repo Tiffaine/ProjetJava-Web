5b5d4b5662ba68c9cb0afe3b1a85b3cc646fd80a
PA-Delaunay <40235634+PA-Delaunay@users.noreply.github.com>
2019-01-24 11:05:48 +0100

    Update retourInscription.php

--- retourInscription.php
+++ retourInscription.php
@@ -6,57 +6,60 @@
 		<title> Page d'inscription </title> 
 	</head> 
 	<body> 
-		<?php // start a session 
-			//session_start();
-			if(isset($_POST["login"]) and 
-			   isset($_POST["mdp"]) and 
-			   isset($_POST["validate"]) and
-			   isset($_POST["nom"]) and 
-			   isset($_POST["prenom"]) and 
-			   isset($_POST["dateN"])){
-				$login = $_POST["login"];
-				var_dump($login);
-				$mdp = $_POST["mdp"];
-				var_dump($mdp);
-				$validate = $_POST["validate"];
-				var_dump($validate);
-				$nom = $_POST["nom"];
-				var_dump($nom);
-				$prenom = $_POST["prenom"];
-				var_dump($prenom);
-				$dateN = $_POST["dateN"];
-				var_dump($dateN);
-				if($mdp==$validate){
-					echo("code bon");
-					$pgsql_conn = pg_connect("dbname=dondusang host=localhost user=admin password=projetgroupe4");
-					if(!$pgsql_conn){
-						echo("connexion eronnee");
-						echo(pg_last_error($pgsql_conn));
-						exit();
-					} else {
-						echo("connexion erronee");
-					}
-					$sql_query = "select login from Utilisateur u where u.login like '".$login"';";
-					echo($sql_query);
-					$sql_login = pg_query($pgsql_conn, $sql_query);
-					echo($sql_login);
-					if(pg_num_rows($sql_login) == '0'){
-						echo("On peut inscrire la personne");
-					} else {
-						echo ("Login déjà utilisé !");
-						echo ("<a href=\"inscription.php\"> Retour</a>");
-						exit();
-					}
-				} else {
-					echo ("<a href=\"inscription.php\"> Veuillez valider votre mot de passe </a>");
-					exit();
-				}
+		<br> Tentative d inscription </br>
+	</body>
+	<?php 
+	// start a session 
+	//session_start();
+	echo("Coucou\n");
+	if(isset($_POST["login"]) and 
+		isset($_POST["mdp"]) and 
+		isset($_POST["validate"]) and
+		isset($_POST["nom"]) and 
+		isset($_POST["prenom"]) and 
+		isset($_POST["dateN"])){
+		$login = $_POST["login"];
+		var_dump($login);
+		$mdp = $_POST["mdp"];
+		var_dump($mdp);
+		$validate = $_POST["validate"];
+		var_dump($validate);
+		$nom = $_POST["nom"];
+		var_dump($nom);
+		$prenom = $_POST["prenom"];
+		var_dump($prenom);
+		$dateN = $_POST["dateN"];
+		var_dump($dateN);
+		if($mdp==$validate){
+			echo("code bon");
+			$pgsql_conn = pg_connect("dbname=dondusang host=localhost user=admin password=projetgroupe4");
+			if(!$pgsql_conn){
+				echo("connexion eronnee");
+				echo(pg_last_error($pgsql_conn));
+				exit();
 			} else {
-				echo ("<a href=\"inscription.php\"> Saisie incorrecte</a>");
+				echo("connexion erronee");
+			}
+			$sql_query = "select login from Utilisateur u where u.login like '".$login"';";
+			echo($sql_query);
+			$sql_login = pg_query($pgsql_conn, $sql_query);
+			echo($sql_login);
+			if(pg_num_rows($sql_login) == '0'){
+				echo("On peut inscrire la personne");
+			} else {
+				echo ("Login déjà utilisé !");
+				echo ("<a href=\"inscription.php\"> Retour</a>");
 				exit();
 			}
-		?> 
-	</body>
+		} else {
+			echo ("<a href=\"inscription.php\"> Veuillez valider votre mot de passe </a>");
+			exit();
+		}
+	} else {
+		echo ("<a href=\"inscription.php\"> Saisie incorrecte</a>");
+		exit();
+	}
+	?> 
 </html>
 
 
