c8519cb3a382c6ef2e005a955018fe39eb413034
PA-Delaunay <40235634+PA-Delaunay@users.noreply.github.com>
2019-01-24 10:48:23 +0100

    Update inscription.php

--- inscription.php
+++ inscription.php
@@ -7,33 +7,34 @@
 	</head> 
 	<body> 
 		<br> Formulaire d'Inscription </br>
-		<form method ="get" action ="retourInscription.php"> 
+	</body>
+		<form method ="post" action ="retourInscription.php"> 
 			<fieldset> 
 				<legend>Login : </legend>
-                <input type = "text" name="login" />
-            </fieldset>
-            <fieldset>
-            	<legend>Mot de passe : </legend>
-                <input type = "password" name="mdp" />
-            </fieldset>
-            <fieldset>
-            	<legend>Validation : </legend>
-                <input type = "password" name="validate" />
-            </fieldset>
-            <fieldset>
-            	<legend>Nom : </legend>
-                <input type = "text" name="nom" />
-            </fieldset>
-            <fieldset>
-            	<legend> Prenom : </legend>
-                <input type = "text" name="prenom" />
-            </fieldset>
-            <fieldset>
-            	<legend> Date de naissance : </legend>
-                <input type = "text" name="dateN" />
-            </fieldset>
-            	<input type ="submit" value ="connexion" /> 
-		</form>
+                		<input type = "text" name="login" />
+            		</fieldset>
+           		<fieldset>
+            			<legend>Mot de passe : </legend>
+                		<input type = "password" name="mdp" />
+            		</fieldset>
+            		<fieldset>
+            			<legend>Validation : </legend>
+                		<input type = "password" name="validate" />
+            		</fieldset>
+            		<fieldset>
+            			<legend>Nom : </legend>
+                		<input type = "text" name="nom" />
+            		</fieldset>
+            		<fieldset>
+            			<legend> Prenom : </legend>
+                		<input type = "text" name="prenom" />
+            		</fieldset>
+            		<fieldset>
+            			<legend> Date de naissance : (AAAA-MM-JJ) </legend>
+                		<input type = "text" name="dateN" />
+            		</fieldset>
+            			<input type ="submit" name ="submit" value ="creer un compte"/> 
+			</form>
 		<a href ='index.php'> Retour </a>
-	</body>
 </html>
+
